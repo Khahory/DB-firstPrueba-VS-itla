5b8e13adbf035779b0787f10237df6e8747181d5
KhahoryITLA <20164247@itla.edu.do>
2019-06-08 20:00:54 -0400

    Commit

---
+++ DB firstPrueba/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB_firstPrueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (BaseDeDatoNuevaEntities baseDeDatoNuevaEntities = new BaseDeDatoNuevaEntities())
+            {
+                Recipe recipe = new Recipe
+                    (
+                        
+                    ); 
+
+               
+            }
+
+
+                return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
